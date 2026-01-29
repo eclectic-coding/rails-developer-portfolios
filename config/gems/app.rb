8d94bf8b12e732f62cbc9d0f36804f8dcbac7a57
Chuck Smith <eclectic-coding@users.noreply.github.com>
2026-01-29 10:24:22 -0500

    Initial_commit

---
+++ config/gems/app.rb
@@ -0,0 +1,18 @@
+gem "annotate"
+gem "cssbundling-rails"
+gem "inline_svg"
+gem "name_of_person"
+# gem "strong_migrations" # Uncomment if you want to use strong_migrations
+
+group :development, :test do
+  gem "erb_lint"
+  gem "faker"
+end
+
+group :development do
+  gem "bundle-audit", require: false
+  gem "hotwire-spark"
+  # gem "bullet" # Uncomment if you want to use Bullet
+  gem "letter_opener_web"
+  gem "rails-erd"
+end
