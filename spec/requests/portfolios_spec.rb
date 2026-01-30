3c00edc5372bf0e4e49f01d792d8e1af77eb3045
Chuck <chuck@eclecticsaddlebag.com>
2026-01-30 06:42:07 -0500

    Add rspec tests

---
+++ spec/requests/portfolios_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe PortfoliosController, type: :request do
+  describe 'GET /portfolios' do
+    let(:sample_data) do
+      [
+        { 'name' => 'John Doe', 'link' => 'https://johndoe.com' },
+        { 'name' => 'Jane Smith', 'link' => 'https://janesmith.com' }
+      ]
+    end
+
+    before do
+      allow(DeveloperPortfoliosFetcher).to receive(:fetch).and_return(sample_data)
+    end
+
+    context 'when requesting HTML' do
+      it 'returns success' do
+        get '/portfolios'
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'renders the portfolios page' do
+        get '/portfolios'
+        expect(response.body).to include('Developer Portfolios')
+      end
+    end
+
+    context 'when requesting JSON' do
+      it 'returns JSON data' do
+        get '/portfolios.json'
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to match(/application\/json/)
+      end
+
+      it 'returns portfolios as JSON' do
+        get '/portfolios.json'
+        json_response = JSON.parse(response.body)
+        expect(json_response).to eq(sample_data)
+      end
+    end
+  end
+end
