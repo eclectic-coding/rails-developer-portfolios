8bcc5af1d4eddc32be82d276cd2b625b662bd0fa
Chuck Smith <eclectic-coding@users.noreply.github.com>
2026-01-30 06:44:14 -0500

    Update tests

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -6,23 +6,12 @@ RSpec.describe "Statics", type: :request do
       get static_home_path
       expect(response).to have_http_status(:success)
     end
-
-    it "displays the home page content" do
-      get static_home_path
-      expect(response.body).to include("Static#home")
-    end
   end
 
   describe "GET /" do
     it "returns http success" do
       get root_path
       expect(response).to have_http_status(:success)
-    end
-
-
-    it "displays the home page content" do
-      get root_path
-      expect(response.body).to include("Static#home")
     end
   end
 end
